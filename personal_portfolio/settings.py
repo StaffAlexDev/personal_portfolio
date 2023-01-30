c6d32aad069021a2504fd92cc146a206cd9c1bb4
StaffAlexDev <scherbakov.alexeii@gmail.com>
2023-01-30 20:45:25 +0200

    new local settings!

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -127,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+try:
+    from .local_settings import *
+except ImportError:
+    print('You must be a production')
